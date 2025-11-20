3eac43e3c5422da89b7017781e35692051effe8d
PEINGER33 <ozgur.tas03@gmail.com>
2025-11-20 16:48:49 +0100

    Ajout de bouton et mise en forme propre de Katanakake et Trousseau

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,121 +15,13 @@ use App\DataFixtures\MemberFixtures;
 
 class AppFixtures extends Fixture implements DependentFixtureInterface
 {
-    
-    /* Version des Fixtures fonctionnelle avant  
-    
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
 
-        $this->loadTrousseau($manager);
-        $this->loadKatana($manager);
-        $this->loadMember($manager);
-    }
-    
-    private function loadTrousseau(ObjectManager $manager)
-    {
-        
-        foreach ($this->getTrousseauData() as [$description]) {
-            $trousseau = new Trousseau();
-            $trousseau->setDescription($description);
-            $manager->persist($trousseau);
-        }
-        $manager->flush();
-    }
-    
-    private function getTrousseauData()
-    {
-        // trousseau = [description];
-        yield ['Lot de katana 1'];
-        yield ['Lot de katana 2'];
-        yield ['Lot de katana 3'];
-        
-    }
-    
-    
-    private function loadKatana(ObjectManager $manager)
-    { 
-        
-        // Récupère les trousseaux créés
-        $trousseaux = $manager->getRepository(Trousseau::class)->findAll();
-        
-        foreach ($this->getKatanaData() as [$description, $type, $longueur, $trousseau_id]) {
-            $katana = new Katana();
-            $katana->setDescription($description);
-            $katana->setType($type);
-            $katana->setLongueur($longueur);
-            
-            $katana->setTrousseau($trousseaux[$trousseau_id]);
-
-            
-            $manager->persist($katana);
-        }
-        $manager->flush();
-    }
- 
-    private function getKatanaData()
-    {
-        // katana = [description, type, longueur, trousseau_id];
-        yield ['Honjo Masamune','Tachi', 71.0, 0];
-        yield ['Kusanagi-no-Tsurugi', 'Ken', 72.0, 0];
-        yield ['Muramasa', 'Shinogi Zukuri ', 72.0, 1];
-        yield ['Mikazuki Munechika', 'Tachi', 80.0, 1];
-        yield ['Dōjigiri Yasutsuna', 'Tachi', 80.0, 1];
-        yield ['Ōdenta Mitsuyo', 'Tachi',  82.0, 2];
-        yield ['Juzumaru Tsunetsugu', 'Tachi', 81.0, 2];
-        yield ['Onimaru Kunitsuna', 'Tachi', 77.0, 2];
-    }
-    
-    # Partie relatif aux Member
-    
-    private UserPasswordHasherInterface $hasher;
-    
-    public function __construct(UserPasswordHasherInterface $hasher)
-    {
-        $this->hasher = $hasher;
-    }
-    
-    // /**
-      * Generates initialization data for members :
-      *  [email, plain text password]
-      * @return \\Generator
-     // 
-    private function membersGenerator()
-    {
-        yield ['olivier@localhost','123456'];
-        yield ['slash@localhost','123456'];
-    }
-    
-    //...
-    public function loadMember(ObjectManager $manager): void
-    {
-        foreach ($this->membersGenerator() as [$email, $plainPassword]) {
-            $user = new Member();
-            $password = $this->hasher->hashPassword($user, $plainPassword);
-            $user->setEmail($email);
-            $user->setPassword($password);
-            
-            // $roles = array();
-            // $roles[] = $role;
-            // $user->setRoles($roles);
-            
-            $manager->persist($user);
-        }
-        $manager->flush();
-    }
-    
-    */
-    
-    
     // Nouvelle version des Fixtures 
     
-    // defines reference names for instances of Trousseau
+    // definition des references 
     private const OLIVIER_TROUSSEAU = 'olivier-trousseau';
     private const SLASH_TROUSSEAU   = 'slash-trousseau';
-    //private const OLIVIER_MEMBER = 'member_olivier';
-    //private const SLASH_MEMBER   = 'member_slash';
+
     private const OLIVIER_KATANAKAKE = 'katanakake_olivier';
     private const SLASH_KATANAKAKE   = 'katanakake_slash';
     
@@ -154,28 +46,15 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
         ];
     }
     
-    /* private static function membersDataGenerator()
-    {
-        yield ['olivier@localhost', '123456', 'ROLE_ADMIN', self::OLIVIER_TROUSSEAU, self::OLIVIER_MEMBER];
-        yield ['slash@localhost', '123456', 'ROLE_USER',  self::SLASH_TROUSSEAU, self::SLASH_MEMBER];
-    } */
-    
-    
-    /**
-     * Generates initialization data for trousseaux : [description, reference]
-     * @return \Generator
-     */
+
+
     private static function trousseauxDataGenerator()
     {
         yield ['Lot de katana 1', self::OLIVIER_TROUSSEAU];
         yield ['Lot de katana 2', self::SLASH_TROUSSEAU];
     }
     
-    /**
-     * Generates initialization data for katanas :
-     * [trousseau reference, description, type, longueur]
-     * @return \Generator
-     */
+
     private static function katanasDataGenerator()
     {
         yield [self::OLIVIER_TROUSSEAU, 'Honjo Masamune', 'Tachi', 71.0,'honjo.jpg',  self::KATANA_HONJO];
@@ -184,11 +63,7 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
         yield [self::SLASH_TROUSSEAU, 'Mikazuki Munechika', 'Tachi', 80.0,'mikazuki.jpg', self::KATANA_MIKAZUKI];
     }
     
-    /**
-     * Generates initialization data for katanakake :
-     * [description, publiee, createur reference]
-     * @return \Generator
-     */
+
     private static function katanakakeDataGenerator()
     {
         yield ["Collection d'Olivier", true, MemberFixtures::OLIVIER_MEMBER,  self::OLIVIER_KATANAKAKE,  [self::KATANA_HONJO, self::KATANA_KUSANAGI]];
@@ -214,26 +89,6 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
             $this->addReference($trousseauRef, $trousseau);
         }
         
-        //  création des membres, liés à leur trousseau
-        
-        /* foreach (self::membersDataGenerator() as [$email, $plainPassword, $role ,$trousseauRef, $memberRef]) {
-            $member = new Member();
-            $member->setEmail($email);
-            $member->setPassword($this->hasher->hashPassword($member, $plainPassword));
-            
-            $roles = array();
-            $roles[] = $role;
-            $member->setRoles($roles);
-            
-            // récupère le trousseau correspondant (comme dans le code du prof)
-            $trousseau = $this->getReference($trousseauRef, Trousseau::class);
-            $member->setTrousseau($trousseau);
-            
-            $manager->persist($member);
-            $this->addReference($memberRef, $member);
-            $manager->flush();
-            
-        } */
         
         //  création des katanas, associés à leur trousseau
         foreach (self::katanasDataGenerator() as [$trousseauRef, $desc, $type, $longueur, $imageName, $katanaRef ]) {
@@ -273,9 +128,9 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
         
         // Lier les membres à leurs trousseaux
         foreach (self::memberTrousseauLinks() as [$memberRef, $trousseauRef]) {
-            /** @var Member $member */
+
             $member = $this->getReference($memberRef, Member::class);
-            /** @var Trousseau $trousseau */
+
             $trousseau = $this->getReference($trousseauRef, Trousseau::class);
             
             $member->setTrousseau($trousseau);
